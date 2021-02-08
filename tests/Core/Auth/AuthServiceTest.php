05d13b88725fb2d2c9993e0c1d5d99a6f67a2bd7
Sixto Ortega <ortegasixto7@gmail.com>
2021-02-08 18:07:21 -0400

    Adding edit tests

--- tests/Core/Auth/AuthServiceTest.php
+++ tests/Core/Auth/AuthServiceTest.php
@@ -104,7 +104,140 @@ class AuthServiceTest extends TestCase
     $this->assertTrue($result->getFirstName() === $user->getFirstName());
     $this->assertTrue($result->getLastName() === $user->getLastName());
     $this->assertTrue($result->getEmail() === $user->getEmail());
-    $this->assertTrue($authService->isPasswordValid($user->getPassword(), $result->getPassword()));
-    $this->assertTrue($authService->isSecurityCodeValid($user->getSecurityCode(), $result->getSecurityCode()));
+    $this->assertTrue($authService->isPasswordValid('123456', $result->getPassword()));
+    $this->assertTrue($authService->isSecurityCodeValid('123456', $result->getSecurityCode()));
+  }
+
+  public function test_AuthService_Edit_WithEmptyFirstName_Returns_Exception()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setFirstName('   ');
+
+    try {
+      $authService->update($data);
+    } catch (Exception $ex) {
+      $this->assertTrue($ex instanceof Exception);
+    }
+  }
+
+  public function test_AuthService_Edit_WithEmptyLastName_Returns_Exception()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setLastName('   ');
+
+    try {
+      $authService->update($data);
+    } catch (Exception $ex) {
+      $this->assertTrue($ex instanceof Exception);
+    }
+  }
+
+  public function test_AuthService_Edit_WithEmptyEmail_Returns_Exception()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setEmail('   ');
+
+    try {
+      $authService->update($data);
+    } catch (Exception $ex) {
+      $this->assertTrue($ex instanceof Exception);
+    }
+  }
+
+  public function test_AuthService_Edit_WithEmptyPassword_Returns_Exception()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setPassword('   ');
+
+    try {
+      $authService->update($data);
+    } catch (Exception $ex) {
+      $this->assertTrue($ex instanceof Exception);
+    }
+  }
+
+  public function test_AuthService_Edit_WithEmptySecurityCode_Returns_Exception()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setSecurityCode('   ');
+
+    try {
+      $authService->update($data);
+    } catch (Exception $ex) {
+      $this->assertTrue($ex instanceof Exception);
+    }
+  }
+
+  public function test_AuthService_Edit_WithCorrectData_Returns_SameData()
+  {
+    $authService = new AuthService(new AuthInMemoryRepository());
+    $user = new User();
+    $user->setFirstName('FirstName');
+    $user->setLastName('LastName');
+    $user->setEmail('email@example.com');
+    $user->setPassword('123456');
+    $user->setSecurityCode('123456');
+    $authService->save($user);
+
+    $data = $authService->findOneByEmail('email@example.com');
+    $data->setFirstName('FirstName1');
+    $data->setLastName('LastName1');
+    $data->setPassword('12345678');
+    $data->setSecurityCode('12345678');
+
+    $authService->update($data);
+
+    $result = $authService->findOneByEmail($user->getEmail());
+    $this->assertTrue($result !== null);
+    $this->assertTrue($result->getFirstName() === $user->getFirstName());
+    $this->assertTrue($result->getLastName() === $user->getLastName());
+    $this->assertTrue($result->getEmail() === $user->getEmail());
+    $this->assertTrue($authService->isPasswordValid('12345678', $result->getPassword()));
+    $this->assertTrue($authService->isSecurityCodeValid('12345678', $result->getSecurityCode()));
   }
 }
